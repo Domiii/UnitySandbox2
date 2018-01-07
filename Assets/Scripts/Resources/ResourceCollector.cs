eccf304a496f4d7130c3e7e57396fe832805fe15
Domiii <dominik.seifert@gmail.com>
2018-01-07 21:03:06 +0800

    - added basic resource collector

---
+++ Assets/Scripts/Resources/ResourceCollector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceCollector : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		var resource = other.GetComponent<ResourceObject> ();
+		if (resource) {
+			resource.Consume ();
+		}
+	}
+}
